b7996425d5154299948653698c636272f08a381b
ahmed <ahmednas_alani@yahoo.com>
2020-09-01 10:37:09 -0700

    parcticing Angular Binding

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+export class ServerComponent implements OnInit {
+  allowNewServer = false;
+  noServerCreater = 'no service work';
+  data = "";
+  server=""
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000)
+   }
+
+  ngOnInit(): void {
+  }
+  onServerCreater(){
+    this.noServerCreater = 'server creater is working'+" " +this.data ;
+
+  }
+
+  onUpdate(event:Event){
+
+    this.data = (<HTMLInputElement>event.target).value;
+
+
+  }
+
+
+
+}
